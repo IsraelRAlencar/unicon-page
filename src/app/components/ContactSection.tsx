17bc7c0141736e7d1447f52ba14c0535000f65d2
IsraelRAlencar <israelalencarribeiro@gmail.com>
2025-06-12 15:19:11 -0400

    fix - really fixing maps

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -207,7 +207,7 @@ export default function ContactSection() {
               <CardContent className="p-0">
                 <div className="h-64 bg-muted flex items-center justify-center">
                   <iframe
-                    src="https://www.google.com/maps/place/Unicon+Consultoria+e+Contabilidade/data=!4m2!3m1!1s0x0:0x42fd0e2012c6d0fd?sa=X&ved=1t:2428&ictx=111"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3693.4206122604855!2d-54.7859902!3d-22.2241146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9489a829d2244007%3A0x42fd0e2012c6d0fd!2sUnicon%20Consultoria%20e%20Contabilidade!5e0!3m2!1spt-BR!2sbr!4v1749755820479!5m2!1spt-BR!2sbr"
                     width="100%"
                     height="256"
                     style={{ border: 0 }}
@@ -229,7 +229,7 @@ export default function ContactSection() {
               <CardContent className="p-0">
                 <div className="h-64 bg-muted flex items-center justify-center">
                   <iframe
-                    src="www.google.com/maps/place/UNICON+-+Consultoria,+Auditoria+e+Contabilidade/data=!4m2!3m1!1s0x0:0x9e7d6cfeb598d648?sa=X&ved=1t:2428&ictx=111"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3693.4206122604855!2d-54.7859902!3d-22.2241146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9489a829d2244007%3A0x42fd0e2012c6d0fd!2sUnicon%20Consultoria%20e%20Contabilidade!5e0!3m2!1spt-BR!2sbr!4v1749755820479!5m2!1spt-BR!2sbr"
                     width="100%"
                     height="256"
                     style={{ border: 0 }}
